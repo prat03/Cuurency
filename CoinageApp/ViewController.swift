30654d50cc0613d2bd6f30cead0d8e054eb309a8
leena <leenajai07@gmail.com>
2023-06-08 10:03:37 +0530

    changes on app

--- CoinageApp/ViewController.swift
+++ CoinageApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Lets do it")
+        print("currency")
     }
 
 
