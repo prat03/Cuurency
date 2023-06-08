2bfd17b987d84a8e6eede869d5c910c16e5d469f
Prathiba N <nprathiba30@gmail.com>
2023-06-08 16:17:19 +0530

    Interface align

--- SME_Project/ViewController.swift
+++ SME_Project/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("final!!!")
+        
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.frame = view.bounds
+//        gradientLayer.colors = [#colorLiteral(red: 0, green: 0.5725490196, blue: 0.2705882353, alpha: 1).cgColor, UIColor(red: 252/255, green: 238/255, blue: 33/255, alpha: 1).cgColor]
+//        gradientLayer.shouldRasterize = true
+//        backgroundGradientView.layer.addSublayer(gradientLayer)
     }
 
 
