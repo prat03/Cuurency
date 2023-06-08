647841e92283322937dacb829544fc987eecd09e
leena <leenajai07@gmail.com>
2023-06-08 13:40:40 +0530

    Initial Commit

---
+++ SME_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SME_Project
+//
+//  Created by Leena, Jayakumar (Contractor) on 08/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
